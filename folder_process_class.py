b43847a17de21b5a2e755dd3226a579591d2fdc8
capwindernie <capwindernie@lenovo_win11.com>
2024-07-12 15:23:07 +0800

    revise some methods

--- folder_process_class.py
+++ folder_process_class.py
@@ -1,12 +1,16 @@
 import os
 import shutil
+import datetime
 
 
 class FolderProcess:
     @classmethod
     def folder_move(cls, src_dir, dst_dir):
-        if os.path.exists(src_dir) and os.path.basename(src_dir) not in os.listdir(dst_dir):
+        if not os.path.exists(dst_dir):
             os.makedirs(dst_dir, exist_ok=True)
+            print(f'未找到目录{dst_dir},已自动创建')
+
+        if os.path.exists(src_dir) and os.path.basename(src_dir) not in os.listdir(dst_dir):
             shutil.move(src=str(src_dir), dst=os.path.join(str(dst_dir), os.path.basename(str(src_dir))))
             print(f'已将{os.path.basename(src_dir)}移动至{dst_dir}')
         elif not os.path.exists(src_dir):
@@ -15,22 +19,21 @@ class FolderProcess:
             print(f'{dst_dir}路径下已存在{os.path.basename(src_dir)}')
 
     @classmethod
-    def folder_rename(cls, src_dir, dst_name):
-        if os.path.exists(src_dir) and dst_name not in os.listdir(os.path.dirname(src_dir)):
+    def folder_add_time(cls, src_dir):
+        if os.path.exists(src_dir):
+            now = datetime.datetime.now()
+            now_str = now.strftime('%Y-%m-%d %H_%M_%S')
+            dst_name = os.path.basename(src_dir) + ' ' + now_str
+            print(dst_name)
             directory, filename = os.path.split(src_dir)
             dst_path = os.path.join(directory, dst_name)
             os.rename(src_dir, dst_path)
             print(f'已将{filename}修改为{dst_name}')
         elif not os.path.exists(src_dir):
             print(f'未找到{src_dir}，请输入正确的文件路径!')
-        elif dst_name in os.listdir(os.path.dirname(src_dir)):
-            print(f'{src_dir}路径下已存在{dst_name}')
 
 
 if __name__ == '__main__':
     source_path = "D:\\Code\\src"
-    target_name = "renamed_src"
-    revised_path = os.path.join(os.path.dirname(source_path), target_name)
-    target_dir = "D:\\Code\\dst"
-    FolderProcess.folder_rename(source_path, target_name)
-    FolderProcess.folder_move(revised_path, target_dir)
+    target_dir = "D:\\Code\\dst1"
+    FolderProcess.folder_move(source_path, target_dir)
